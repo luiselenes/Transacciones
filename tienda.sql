7a574c721d19dd899d6397115c78d88db3e65591
Tania Nafarrate <taniabastidas32@gmail.com>
2020-10-05 17:36:13 -0600

    kitense perros k voi a explotar

--- tienda.sql
+++ tienda.sql
@@ -11,11 +11,34 @@ CREATE TABLE Productos
     continente char(1) 
 );
 GO
+create nonclustered index IX_Continente 
+on Productos(continente);
 
 CREATE TABLE ventas (
-folio int not null primary key
-idproducto int not null 
+folio int not null primary key,
+idproducto int not null ,
 noUnidades INT,
 FOREIGN KEY (idproducto) REFERENCES Productos(idproductos)
 );
 GO
+
+create table continentes(
+idcontinente int identity(1,1),
+continente char(1),
+descripcion varchar(50));
+
+create nonclustered index IX_ContinenteContinente
+on continentes(continente)
+
+Create procedure sp_actualiza @Continente char (1)
+as
+Select 1 
+from continentes
+with UPDLOCK where continente=@Continente
+
+
+
+
+
+select * from Productos
+
