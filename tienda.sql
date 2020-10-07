0f0b74f39a8370eec3d13fc842d48404433c0914
luiselenes <luiseelenes@outlook.com>
2020-10-06 19:37:29 -0600

    modificacion con la idea sin resolver

--- tienda.sql
+++ tienda.sql
@@ -22,23 +22,87 @@ FOREIGN KEY (idproducto) REFERENCES Productos(idproductos)
 );
 GO
 
+
 create table continentes(
 idcontinente int identity(1,1),
 continente char(1),
-descripcion varchar(50));
+descripcion varchar(50)
+);
+go
 
 create nonclustered index IX_ContinenteContinente
-on continentes(continente)
+on continentes(continente)	
 
-Create procedure sp_actualiza @Continente char (1)
+-----ate procedure s_actualiza @Continente char (1)
 as
-Select 1 
+Select 1
 from continentes
 with UPDLOCK where continente=@Continente
+----------------------------------------
+
+--Create procedure sp_actualiza @Continente char(1) 
+as
+begin tran 
+Select * from Productos with (UPDLOCK) 
+where continente=1
+
+
+--update Productos set existencia = 113 where idproductos=1
+
+commit tran
+
+end
+-----------------------------------------------
+SELECT TOP (1000) [idproductos]
+      ,[nombre]
+      ,[stockminimo]
+      ,[stockmaximo]
+      ,[existencia]
+      ,[continente]
+  FROM [tienda].[dbo].[Productos]
+
+  Select * from Productos 
+where continente
+
+
+
+v--mostar un cliente con el rfc
+CREATE PROCEDURE sp_selectcliente @rfc nvarchar(13)
+AS
+BEGIN
+	select nombre,edad,idciudad from [Crud].[dbo].[Clientes]
+	where rfc = @rfc
+	;
+end
+go
+--------------------------------
+Create procedure sp_actualiza @Continente char(1)
+as
+begin tran 
+Select * from Productos with (UPDLOCK) 
+where continente=@Continente
+declare  @idproducto int, @exist int , @nonunidades int
+
+
+set @idproducto =(select idproducto from ventas)
+set @nonunidades =(select noUnidades from ventas where idproducto=@idproducto)
+
+
+set @exist = (select existencia from Productos where idproductos=@idproducto)
+
+
+set @exist =@exist-@nonunidades
+update Productos set existencia = @exist where idproductos=
+
+--lect @var1
 
 
 
 
 
-select * from Productos
 
+
+
+
+commit tran
+--update Productos set existencia = 113 where idproductos=1
